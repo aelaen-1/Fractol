772bfc8322fb72edc4d4bf8fc5b559f8265c128c
aloon <118343415+aelaen-1@users.noreply.github.com>
2024-08-02 11:56:39 +0200

    Delete jennet directory

--- jennet/libft/free_utils.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-void	free_and_assign_null(void **p)
-{
-	free(*p);
-	*p = NULL;
-}
--- jennet/utils.c
+++
@@ -1,8 +0,0 @@
-#include "fractol.h"
-
-double	abs_double(double val)
-{
-	if (val < 0)
-		return (-val);
-	return (val);
-}
